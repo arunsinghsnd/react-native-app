29b8bfed27d0b01a10c60734319400b85f300c08
arunsinghsnd <arunsinghsnd@gmail.com>
2021-04-21 23:05:19 +0530

    minor change

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ const App = () => {
             headerStyle: {
               backgroundColor: '#0f4c75',
             },
-            title: 'LOC Netflix App',
+            title: 'Netflix App',
             headerTitleStyle: {
               textAlign: 'center',
               color: '#00b7c2',
@@ -64,7 +64,7 @@ const App = () => {
             headerStyle: {
               backgroundColor: '#0f4c75',
             },
-            title: 'LOC Netflix App',
+            title: 'Netflix App',
             headerTitleStyle: {
               textAlign: 'center',
               color: '#00b7c2',
