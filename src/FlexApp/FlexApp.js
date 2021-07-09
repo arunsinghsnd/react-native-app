39f8b5aff3c6d3ed593166884b6a6aa9d92e51f0
Arun Kumar Singh <arunsinghsnd@gmail.com>
2021-07-09 14:46:19 +0530

    flex demo completed

--- src/FlexApp/FlexApp.js
+++ src/FlexApp/FlexApp.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
 const FlexApp = () => {
   return (
-    <View>
-      <Text>FlexDemo</Text>
+    <View style={styles.container}>
+      <View style={styles.flexOne} />
+      <View style={styles.flexTwo} />
+      <View style={styles.flexThree} />
     </View>
   );
 };
 
 export default FlexApp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  flexOne: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  flexTwo: {
+    flex: 2,
+    backgroundColor: 'darkorange',
+  },
+  flexThree: {
+    flex: 3,
+    backgroundColor: 'green',
+  },
+});
